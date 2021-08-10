c106a8c971c2fafc408bee76f042dac092b556ce
Ivanka1991 <73844703+Ivanka1991@users.noreply.github.com>
2021-08-11 00:28:19 +0300

    fin

---
+++ src/components/Button/Button.js
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Button extends Component {
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+  };
+
+ 
+
+  render() {
+    return (
+      <button 
+      onClick={this.props.onClick} 
+      className="Button" 
+      type="button">
+        Load more
+      </button>
+    );
+  }
+}
+
+export default Button;
+
+
+
+
+
+
+
+/* window.scrollTo({
+    top: document.documentElement.scrollHeight,
+    behavior: 'smooth',
+  }); */
